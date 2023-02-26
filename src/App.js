895d3b7a9cc88f7e4e8c0a2d624af9bb2d8a44a4
sumon paul <sumpaul100@gmail.com>
2023-02-26 10:28:35 +0600

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1 className="bg-violet-700">somethings</h1>
+        <Navbar />
       </header>
     </div>
   );
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div>
+      <h1 className="flex h-[80px] bg-[#0a192f] justify-between px-4 items-center text-white">nav bar</h1>
+    </div>
+  );
+};
+
+export default Navbar;
