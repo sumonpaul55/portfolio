5626b626c4f70bbe84e5a2eca62de9b05b73353c
sumon paul <sumpaul100@gmail.com>
2023-02-26 21:32:16 +0600

    navbar working

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,42 @@
 import React from "react";
-
+import logo from "../assets/logo1.png";
+import { FaBars } from "react-icons/fa";
 const Navbar = () => {
   return (
     <div>
-      <h1 className="flex h-[80px] bg-[#0a192f] justify-between px-4 items-center text-white">nav bar</h1>
+      <div className="flex h-[80px] bg-[#0a192f] justify-between px-4 items-center text-white">
+        <div className="logo">
+          <img src={logo} className="w-[50px]" alt="logo" />
+        </div>
+        {/* menu */}
+        <div className="">
+          <ul className="flex">
+            <li>Home</li>
+            <li>About</li>
+            <li>Experience</li>
+            <li>Skills</li>
+            <li>Work</li>
+            <li>Contact Me</li>
+          </ul>
+        </div>
+        {/* Hamburger */}
+        <div className="hidden">
+          <FaBars />
+        </div>
+        {/* mobile menu */}
+        <div className="hidden">
+          <ul className="">
+            <li>Home</li>
+            <li>About</li>
+            <li>Experience</li>
+            <li>Skills</li>
+            <li>Work</li>
+            <li>Contact Me</li>
+          </ul>
+        </div>
+        {/* social icons */}
+        <div className="hidden"></div>
+      </div>
     </div>
   );
 };
