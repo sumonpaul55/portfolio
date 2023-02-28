ca3485d6377802944a42bab0ecf4f4c6b948e5c7
sumon paul <sumpaul100@gmail.com>
2023-02-28 19:27:38 +0600

    navbar completed with responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../assets/logo1.png";
-import { FaBars } from "react-icons/fa";
+import { RiBarChartHorizontalLine, RiCloseLine } from "react-icons/ri";
 const Navbar = () => {
+  const [nav, setNav] = useState(false);
+
   return (
     <div>
       <div className="flex h-[80px] bg-[#0a192f] justify-between px-4 items-center text-white">
@@ -9,31 +11,27 @@ const Navbar = () => {
           <img src={logo} className="w-[50px]" alt="logo" />
         </div>
         {/* menu */}
-        <div className="">
-          <ul className="flex">
-            <li>Home</li>
-            <li>About</li>
-            <li>Experience</li>
-            <li>Skills</li>
-            <li>Work</li>
-            <li>Contact Me</li>
-          </ul>
-        </div>
+        <ul className="hidden md:flex">
+          <li>Home</li>
+          <li>About</li>
+          <li>Experience</li>
+          <li>Skills</li>
+          <li>Work</li>
+          <li>Contact Me</li>
+        </ul>
         {/* Hamburger */}
-        <div className="hidden">
-          <FaBars />
+        <div onClick={() => setNav(!nav)} className="md:hidden z-10 cursor-pointer text-2xl">
+          {!nav ? <RiBarChartHorizontalLine /> : <RiCloseLine />}
         </div>
         {/* mobile menu */}
-        <div className="hidden">
-          <ul className="">
-            <li>Home</li>
-            <li>About</li>
-            <li>Experience</li>
-            <li>Skills</li>
-            <li>Work</li>
-            <li>Contact Me</li>
-          </ul>
-        </div>
+        <ul className={`${!nav ? "hidden" : "absolute bg-[#0a192f] text-white flex items-center flex-col justify-center inset-0 md:hidden"}`}>
+          <li className="py-4 text-3xl">Home</li>
+          <li className="py-4 text-3xl">About</li>
+          <li className="py-4 text-3xl">Experience</li>
+          <li className="py-4 text-3xl">Skills</li>
+          <li className="py-4 text-3xl">Work</li>
+          <li className="py-4 text-3xl">Contact Me</li>
+        </ul>
         {/* social icons */}
         <div className="hidden"></div>
       </div>
